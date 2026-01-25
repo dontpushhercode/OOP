7a49b8ac61008ad66f31e49b11782fd807577b23
evlit <3vlitv1@gmail.com>
2026-01-25 19:07:14 +0500

    Problem #1

---
+++ OOP/src/practice1/Practice1Class.java
@@ -0,0 +1,11 @@
+package practice1;
+
+public class Practice1Class {
+	public static void main(String[] args) {
+		printMyName();
+	}
+	
+	public static void printMyName() {
+		System.out.println("+---+ \n|Eva|\n+---+");
+	}
+}
